c022906b5f9f6f1d104fb030fc9adc4f6613e93e
Seunghyeon Yang <ysh410@gmail.com>
2022-10-04 22:44:13 +0900

    feat: lec04의 OOP2 파트를 추가한다

--- src/main/java/lec04/OOP02/PropertyMain.java
+++ src/main/java/lec04/OOP02/PropertyMain.java
@@ -6,6 +6,24 @@ public class PropertyMain {
         PropertyOfClass propertyOfClass = new PropertyOfClass();
         PropertyOfClass propertyOfClassName = new PropertyOfClass("커피");
         PropertyOfClass propertyOfClassNamePrice = new PropertyOfClass("바나나", "1500");
+        propertyOfClassNamePrice.enumDataType = PropertyOfClass.EnumDataType.INSERT;
 
+        System.out.println();
+        System.out.println(propertyOfClass.toString());
+        System.out.println(propertyOfClassNamePrice.toString());
+        System.out.println(propertyOfClassNamePrice.toString());
+
+        System.out.println();
+
+        String[] innerClassPropertiesName = PropertyOfClass.InnerClassInfo.innerClassPropertiesName;
+        for (String strVar : innerClassPropertiesName) {
+            System.out.println("strVar : " + strVar);
+        }
+        System.out.println();
+
+        int[] innerClassPropertiesPrice = PropertyOfClass.InnerClassInfo.innerClassPropertiesPrice;
+        for (int intVar : innerClassPropertiesPrice) {
+            System.out.println("intVar : " + intVar);
+        }
     }
 }
